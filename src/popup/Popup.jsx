992d24fc35b9cc141e7e47b18c850fa6a49534bd
Christian Lee <christianlee101@gmail.com>
2025-04-05 23:36:21 -0500

    Fixed imports

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
-import ToggleSwitch from '../toggleSwitch/ToggleSwitch'import { useUser } from '@/contexts/userContext';
+import ToggleSwitch from '../toggleSwitch/ToggleSwitch';
+import { useUser } from '@/contexts/userContext';
 
 import './Popup.css'
 
